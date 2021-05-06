ae63d7588a8c5cd0fcfd52eeb95f38e9e4ea3d0c
Руслан Ефимов <efimovrusl@gmail.com>
2021-05-06 21:07:16 +0300

    +Discrete Math Lab1

---
+++ DiscreteMathLab1/DiscreteMathLab1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiscreteMathLab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+        private void button8_Click(object sender, EventArgs e)
+        { // SWAP
+            String buffer = textBox1.Text;
+            textBox1.Text = textBox2.Text;
+            textBox2.Text = buffer;
+            Solution.Input1 = textBox1.Text.ToCharArray();
+            Solution.Input2 = textBox2.Text.ToCharArray();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = checkInputCorrectness(textBox1.Text);
+            Solution.Input1 = textBox1.Text.ToCharArray();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = checkInputCorrectness(textBox2.Text);
+            Solution.Input2 = textBox2.Text.ToCharArray();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private String checkInputCorrectness(String input)
+        {
+            String filtered = "";
+            foreach (Char c in input.ToCharArray())
+                if (Char.IsLetter(c) && !filtered.Contains(c)) 
+                    filtered += Char.ToLower(c);
+            return filtered;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Solution.Task1();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Solution.Task2();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Solution.Task3();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Solution.Task4();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = Solution.Task5();
+
+        }
+    }
+}
---
+++ DiscreteMathLab1/DiscreteMathLab1/Solution.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// DEBUG WRITELINE
+using System.Diagnostics;
+
+namespace DiscreteMathLab1
+{
+    public static class Solution
+    {
+        private static char[] 
+            input1 = Array.Empty<char>(), 
+            input2 = Array.Empty<char>();
+        public static char[] Input1 { 
+            get => input1; 
+            set
+            {
+                input1 = value;
+                Array.Sort(input1);
+            }
+        }
+        public static char[] Input2 { 
+            get => input2; 
+            set
+            {
+                input2 = value;
+                Array.Sort(input2);
+            }
+        }
+        public static String Task1()
+        { // вхождение
+            bool result = true;
+            foreach (Char c in Input1)
+                if (!Input2.Contains(c)) result = false;
+            return $"{result}";
+        }
+        public static String Task2()
+        { // объединение
+            String output = "";
+            foreach (Char c in Input1) if (!output.Contains(c)) output += c;
+            foreach (Char c in Input2) if (!output.Contains(c)) output += c;
+            Char[] buffer = output.ToArray();
+            Array.Sort(buffer);
+            return new String(buffer);
+        }
+        public static String Task3()
+        { // пересечение
+            String output = "";
+            char[] buffer = new char[Input2.Length];
+            Array.Copy(Input2, buffer, Input2.Length);
+            for (int i = 0; i < Input1.Length; i++)
+                for (int j = 0; j < Input2.Length; j++)
+                    if (buffer[j] == Input1[i])
+                    {
+                        buffer[j] = '#';
+                        output += Input1[i];
+                        break;
+                    }
+            return output;
+        }
+        public static String Task4()
+        { // разность
+            String output = "";
+            foreach (Char c in Input1)
+                if (!Input2.Contains(c)) output += c;
+            return output;
+        }
+        public static String Task5()
+        { // симметричная разность (убираем общие элементы)
+            String output = "";
+            String intersection = Task3();
+            foreach (Char c in Input1.Concat(Input2)) 
+                if (!output.Contains(c) && 
+                    !intersection.Contains(c)) output += c;
+            Char[] buffer = output.ToArray();
+            Array.Sort(buffer);
+            return new String(buffer);
+        }
+    }
+}
